c122784431506ecf940cec33dd499cc706e257be
Defender <defenderofbasic@proton.me>
2024-12-04 09:20:12 -0500

    fix build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        visakanv: resolve(__dirname, 'visakanv.html')
+        search: resolve(__dirname, 'search.html')
       },
     },
   },
